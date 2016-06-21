8057f76bbe3b81e68ddbe57049abda2dac3fd8e7
gerry <weirich@elexis.ch>
2016-06-21 15:27:31 +0200

    initial import

---
+++ base-deps/makep2.sh
@@ -0,0 +1,4 @@
+loc=`pwd`
+
+ java -jar /Users/gerry/Applications/Eclipse-rcp-mars.app/Contents/Eclipse/plugins/org.eclipse.equinox.launcher_*.jar -application org.eclipse.equinox.p2.publisher.FeaturesAndBundlesPublisher -metadataRepository file:${loc}/repository -artifactRepository file:${loc}/repository -source ${loc}/repository -configs gtk.linux.x86
+   
