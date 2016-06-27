bc54c74e1c483c0647efec594d7746f2129137c9
gerry <weirich@elexis.ch>
2016-06-27 16:55:06 +0200

    corrected dir

--- derived-deps/install.sh
+++ derived-deps/install.sh
@@ -4,7 +4,7 @@ P2=${WEBSPACE}/p2/dependencies
 
 mkdir -p ${P2}/${BUILD_NUMBER}
 
-cp -R ungrad-deps/target/repository/* ${P2}/${BUILD_NUMBER}
+cp -R target/repository/* ${P2}/${BUILD_NUMBER}
 
 rm ${P2}/latest
 ln -s ${P2}/${BUILD_NUMBER} ${P2}/latest
